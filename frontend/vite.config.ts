46c8be6e99f9387444d5f0a09b287ac683f15c32
jade <holmes2003dd@gmail.com>
2025-01-14 18:23:00 -0500

    update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig(({
   plugins: [react()],
-  // Set base based on mode (prod or local)
-  base: "/resumeCLI/frontend",
+  base: "https://github.com/j-adia/resumeCLI/",
 }));
 
