49e6ab1811360819e94035568753260e35313879
jade <holmes2003dd@gmail.com>
2025-01-14 18:39:14 -0500

    add HashRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import CLI from "./pages/CLI";
 
 function App() {  
   return (
-    // important! router needs this so it knows where the routes are
-    <BrowserRouter basename="/resumeCLI/">
+    // handle 404 on refresh
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/resume" element={<CLI/>}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
