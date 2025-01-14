66a79eb57dbb9eec531554ae6b4fa60c90b1079f
jade <holmes2003dd@gmail.com>
2025-01-14 18:07:05 -0500

    update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig(() => ({
+export default defineConfig(({
   plugins: [react()],
   // Set base based on mode (prod or local)
-  // base: mode === 'production' ? '/resumeCLI/' : '/',
-  base: '/resumeCLI',
+  base: "/resumeCLI/",
 }));
 
